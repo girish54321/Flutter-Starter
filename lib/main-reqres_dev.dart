7707104eee59ea966a79fbdd5ae1691b84e9a70d
Girish Parate <girishparate41@gmail.com>
2022-02-13 18:02:26 +0530

    Flavor added

---
+++ lib/main-reqres_dev.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:reqres_app/main.dart';
+import 'app.dart';
+import 'flavors.dart';
+
+Future<void> main() async {
+  await GetStorage.init();
+  F.appFlavor = Flavor.REQRES_DEV;
+  runApp(ReqResApp());
+}
